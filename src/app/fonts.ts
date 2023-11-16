bc02fc615de8115f7fecaa11231528cea24faef2
23rdPro <mailolumide@gmail.com>
2023-11-16 18:48:44 +0100

    added new style

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,8 +1,10 @@
 import localFont from "next/font/local";
-import { Inter, Montserrat, Poppins } from 'next/font/google';
+import { Inter, Montserrat, Poppins, Roboto } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'], weight: ['300', '600'] });
 
 export const montserrat = Montserrat({ subsets: ['latin'], weight: ['300', '400', '700']});
 
 export const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '700', '900']});
+
+export const roboto = Roboto({ subsets: ['latin'], weight: ['500', '700'], display: 'swap'})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next';
-import "@/app/styles/globals.css";
-import { inter, poppins, montserrat, } from './fonts';
+// import "@/app/styles/globals.css";
+import "@/app/styles/animate/animate.min.css";
+import "@/app/styles/owlcarousel/assets/owl.carousel.min.css";
+import "@/app/styles/tempusdominus/css/tempusdominus-bootstrap-4.min.css";
+import "@/app/styles/bootstrap.min.css";
+import "@/app/styles/style.css"
+import { inter, roboto } from './fonts';
 import Script from 'next/script';
 
 export const metadata: Metadata = {
@@ -15,8 +20,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} ${montserrat.className}`}>
+      <body className={`${inter.className} ${roboto.className}`}>
         {children}
+        <Script src="https://code.jquery.com/jquery-3.4.1.min.js" strategy="lazyOnload" />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
+        <Script src="wow/wow.min.js" strategy="lazyOnload" />
+        <Script src="easing/easing.min.js" strategy="lazyOnload" />
+        <Script src="waypoints/waypoints.min.js" strategy="lazyOnload" />
+        <Script src="counterup/counterup.min.js" strategy="lazyOnload" />
+        <Script src="owlcarousel/owl.carousel.min.js" strategy="lazyOnload" />
+        <Script src="tempusdominus/js/moment.min.js" strategy="lazyOnload" />
+        <Script src="tempusdominus/js/moment-timezone.min.js" strategy="lazyOnload" />
+        <Script src="tempusdominus/js/tempusdominus-bootstrap-4.min.js" strategy="lazyOnload" />
       </body>
     </html>
   )
