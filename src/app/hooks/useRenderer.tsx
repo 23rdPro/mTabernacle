1ab087f8ca959497536956196b572ce3d28f1174
23rdPro <mailolumide@gmail.com>
2023-12-22 02:50:06 +0100

    added countdown

--- src/app/hooks/useRenderer.tsx
+++
@@ -1,9 +0,0 @@
-import Countdown from "react-countdown";
-
-export const useRenderer = ({ hours, minutes, seconds, completed }: any) => {
-    if (completed) {
-        return null 
-    } else {
-        return <span>{hours}:{minutes}:{seconds}</span>;
-    }
-}
