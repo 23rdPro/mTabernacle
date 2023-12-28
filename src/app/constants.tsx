525688def3f4ccf67f57b98bfb72e823cd204361
23rdPro <mailolumide@gmail.com>
2023-12-28 10:13:10 +0100

    added foreward

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,6 +1,8 @@
 export const BISHOP = "Bishop Bankole Jefferson"
 export const BISHOP2 = "Bishop Jefferson"
 export const BISHOP3 = "Bankole Jefferson"
+export const BDD = "Bishop Bankole Jefferson DD"
+export const CHURCH = "The Mercy Tabernacle"
 export const brief = `
  Bishop Jefferson, a Nigerian with a distinctive gift for teaching the word of Faith, 
  established Mercy Tabernacle more than two decades ago. Since its inception, 
@@ -10,10 +12,15 @@ export const address = `100 Ajayi Road Oke-Ira Ogba, Lagos`
 export const alternateAddress = `100 Ajayi Road, Modupe Roundabout Oke Ira Ogba, Lagos`
 export const mobile = "0803 502 3574"
 export const email = "info@mercy-tabernacle.com"
-export const mission1 = `Upholding exemplary Christianity in integrity, service to God, and humanity,
-righteous living, upright family values, success in life and uncompromised fear of God.`
-export const mission2 = `Upholding exemplary Christianity in integrity, service to God, and humanity, righteous living, 
-upright family values, success in life and uncompromised fear of God.`
+export const mission1 = `
+At The Mercy Tabernacle, our vision is to make a profound impact by relieving humanity from poverty 
+and suffering, facilitating healing, and liberating individuals. We aim to transform lives, shaping 
+them to mirror Christ's image, and raising individuals as pacesetters, role models, and achievers through 
+the teachings of Jesus Christ.`
+
+export const mission2 = `Join us on our mission to uphold a model of exemplary Christianity 
+that emphasizes integrity, dedicated service to God and humanity, righteous living, strong family values, 
+success in life, and an unwavering fear of God.`
 export const mission3 = `Upholding exemplary Christianity in integrity, service to God, and humanity, righteous living, 
 upright family values, success in life and uncompromised fear of God.`
 export const mission4 = `Our Vision is to Impact, Heal and Liberate mankind from poverty and pain, 
