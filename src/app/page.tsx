5d7440c12246d4bbd2c7f16b9429bc738608120e
23rdPro <mailolumide@gmail.com>
2023-12-29 15:57:42 +0100

    added sermon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,7 @@ import { gelasio, playfairDisplay, rosarivo } from "./fonts";
 import Welcome from "./Components/Welcome";
 import Carousel from "./Components/Carousel";
 import Service from "./Components/Service";
+import Sermon from "./Components/Sermon";
 
 const Home = () => {
     const [date, setDate] = React.useState(0)
@@ -107,6 +108,7 @@ const Home = () => {
             <Carousel />
             <Welcome />
             <Service />
+            <Sermon />
         </>
   )
 }
