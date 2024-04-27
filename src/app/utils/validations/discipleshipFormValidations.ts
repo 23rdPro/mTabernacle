4b4e13e817df5ba2d992c143d6329eaa9f9b52bf
23rdPro <mailolumide@gmail.com>
2024-04-27 14:38:45 +0100

    minor

--- src/app/utils/validations/discipleshipFormValidations.ts
+++
@@ -1,9 +0,0 @@
-import React, { FormEvent } from 'react'
-import * as Yup from 'yup'
-
-export const discipleshipFormSchema = Yup.object({
-  fullName: Yup.string().required("Full name is required"),
-  email: Yup.string().email("Invalid email").required('Email is required'),
-  mobile: Yup.string().required("Phone number is required"),
-  message: Yup.string().notRequired()
-});
