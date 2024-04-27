116581b86a02909ad0660974297b2200e83c0cc5
23rdPro <mailolumide@gmail.com>
2024-04-27 14:14:46 +0100

    minor commit

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,48 +1,48 @@
-import axios from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import axios from 'axios';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function POST(
-    req: NextApiRequest, 
-    res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    const { fullName, email, mobile, message } = req.body;
+// export async function POST(
+//     req: NextApiRequest, 
+//     res: NextApiResponse
+// ) {
+//   if (req.method === 'POST') {
+//     const { fullName, email, mobile, message } = req.body;
 
-    fullName && fullName.length >= 2 
-      ? null 
-      : () => { 
-        throw new Error("Please provide your full name") 
-      }
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    isEmailValid ? null : () => { throw new Error("Please your email address") }
+//     fullName && fullName.length >= 2 
+//       ? null 
+//       : () => { 
+//         throw new Error("Please provide your full name") 
+//       }
+//     const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+//     isEmailValid ? null : () => { throw new Error("Please your email address") }
 
-    // Send data to Contentful
-    try {
-      const response = await axios.post(
-        `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/master/entries`,
-        {
-          fields: {
-            name: { 'en-US': fullName },
-            email: { 'en-US': email },
-            phone: { 'en-US': mobile},
-            message: { 'en-US': message}
-          },
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+//     // Send data to Contentful
+//     try {
+//       const response = await axios.post(
+//         `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/master/entries`,
+//         {
+//           fields: {
+//             name: { 'en-US': fullName },
+//             email: { 'en-US': email },
+//             phone: { 'en-US': mobile},
+//             message: { 'en-US': message}
+//           },
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY}`,
+//             'Content-Type': 'application/json',
+//           },
+//         }
+//       );
 
-      console.log('Contentful response:', response.data);
-      res.status(200).json({ success: true });
-    } catch (error) {
-      console.error('Error sending data to Contentful:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
-  }
-}
+//       console.log('Contentful response:', response.data);
+//       res.status(200).json({ success: true });
+//     } catch (error) {
+//       console.error('Error sending data to Contentful:', error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method Not Allowed' });
+//   }
+// }
